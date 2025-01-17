9f199b2c2c472381323dd63e2dc2fd844341e199
kimhyojung <706shin1728@naver.com>
2025-01-18 02:24:31 +0900

    chore: prettier-plugin-tailwind 추가

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
+  plugins: [
+    require('tailwindcss'),
+    require('prettier-plugin-tailwindcss')
+  ]
+}
+export default config
