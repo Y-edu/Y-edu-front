e6c835b4ac1c879c88ec31e1ee44d26683646025
kimhyojung <706shin1728@naver.com>
2025-01-18 16:27:53 +0900

    chore: mocks,tests폴더 빌드 시 제외하게 설정

--- __mocks__/handler.ts
+++ __mocks__/handler.ts
@@ -2,7 +2,7 @@ import { HttpResponse, http } from 'msw'
 
 export const handlers = [
   // 테스트용 모든 핸들러 정의
-  http.get('http://localhost:3000/api/users', async () => {
+  http.get('http://localhost:3000/api/users', () => {
     return HttpResponse.json({
       id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
       firstName: 'John',
