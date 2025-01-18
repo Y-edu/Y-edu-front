d06e741d96147a9211b30bc89607c865bf4f2df9
kimhyojung <706shin1728@naver.com>
2025-01-18 17:49:59 +0900

    chore: 프로덕션 환경에서만 ignore-loader 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.module.rules?.push(
-      {
-        test: /__mocks__/,
-        loader: 'ignore-loader'
-      },
-      {
-        test: /__tests__/,
-        loader: 'ignore-loader'
-      }
-    )
+    if (process.env.NODE_ENV === 'production') {
+      config.module.rules?.push(
+        {
+          test: /__mocks__/,
+          loader: 'ignore-loader'
+        },
+        {
+          test: /__tests__/,
+          loader: 'ignore-loader'
+        }
+      )
+    }
     return config
   },
   experimental: { instrumentationHook: true }
