1f4513ba4b354c5ea1ea438e9c350ad81c4cdf66
codekattt <widkid00@naver.com>
2025-01-20 00:48:59 +0900

    chore: ESLint 설정에 Prettier 플러그인 추가

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 
-export default function Home () {
+export default function Home() {
   return (
-    <div className='p-4 pt-2 font-pretendard'>
-      <h1 className='rounded bg-blue-500 px-4 py-2 font-pretendard text-base font-bold text-white'>
+    <div className="p-4 pt-2 font-pretendard">
+      <h1 className="rounded bg-blue-500 px-4 py-2 font-pretendard text-base font-bold text-white">
         아아
       </h1>
-      asdasd
+      <button className="m-2 rounded border-2 border-black px-4 py-2 text-black hover:bg-black hover:text-white">
+        mock 데이터 가져오기
+      </button>
     </div>
-  )
+  );
 }
