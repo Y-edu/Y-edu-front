5b64ff18366e6b4bbfdb20c5ff7191886b5fb243
kimhyojung <706shin1728@naver.com>
2025-01-21 19:23:12 +0900

    feat: 사이드바 컴포넌트 구현

---
+++ app/ui/Sidebar.tsx
@@ -0,0 +1,44 @@
+"use client";
+import LogoImage from "../../public/images/logo.png";
+import Image from "next/image";
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
+
+export function Sidebar() {
+  const activeSegment = useSelectedLayoutSegment();
+
+  const activeLinkClassName = "font-bold text-[#527DFF]";
+  return (
+    <nav className="h-screen w-[180px] border-r border-[#E6EFF5] text-[#B1B1B1]">
+      <div className="flex h-[150px] items-center p-4">
+        <Image src={LogoImage} height={36} width={36} alt="로고 이미지" />
+        <span className="ml-2 text-lg font-bold text-[#343C6A]">Y-Edu</span>
+      </div>
+
+      <div className="mt-4 flex flex-col gap-4 p-4">
+        <Link
+          href="/"
+          className={activeSegment === null ? activeLinkClassName : ""}
+        >
+          매칭관리
+        </Link>
+        <Link
+          href="/teacher-management"
+          className={
+            activeSegment === "teacher-management" ? activeLinkClassName : ""
+          }
+        >
+          선생님관리
+        </Link>
+        <Link
+          href="/settle-management"
+          className={
+            activeSegment === "settle-management" ? activeLinkClassName : ""
+          }
+        >
+          정산관리
+        </Link>
+      </div>
+    </nav>
+  );
+}
