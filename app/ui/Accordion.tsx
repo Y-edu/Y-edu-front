d31236768a83d292a58ebf99b16a54d957f868e0
leehyewon0531 <ifpffk@naver.com>
2025-01-23 19:52:49 +0900

    feat: Accordion 펼치기/접기 기능 + 기본 스타일 작업

--- app/ui/Accordion.tsx
+++ app/ui/Accordion.tsx
@@ -1,5 +1,24 @@
+"use client";
+import { useState } from "react";
+
 function Accordion() {
-  return <div className="h-24 w-4/5 bg-white"></div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="relative w-11/12 rounded-3xl border border-gray-300 bg-white">
+      <div className="relative min-h-24 w-full">
+        <button
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+          className="absolute bottom-2 right-4 text-blue-400"
+        >
+          {isOpen ? "접기" : "펼치기"}
+        </button>
+      </div>
+      <div className={`${isOpen ? "h-full" : "h-0"}`}></div>
+    </div>
+  );
 }
 
 export default Accordion;
