ae235a2e3391fbf5585a61994834fc7a8d1c1547
leehyewon0531 <ifpffk@naver.com>
2025-01-23 19:03:15 +0900

    style: 매칭관리 페이지 스타일 일부 수정

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,3 +1,9 @@
+import Accordion from "../ui/Accordion";
+
 export default function Home() {
-  return <div className="p-4 pt-2 font-pretendard"></div>;
+  return (
+    <div className="ml-[180px] p-4 pt-2 font-pretendard">
+      <Accordion />
+    </div>
+  );
 }
