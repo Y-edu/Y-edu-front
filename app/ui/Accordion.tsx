2ceda2bae04a23075614d781a04ee854db2d05da
leehyewon0531 <ifpffk@naver.com>
2025-01-23 19:01:09 +0900

    feat: Accordion 기본 틀 잡기

---
+++ app/ui/Accordion.tsx
@@ -0,0 +1,5 @@
+function Accordion() {
+  return <div className="h-24 w-4/5 bg-white"></div>;
+}
+
+export default Accordion;
--- app/ui/index.ts
+++ app/ui/index.ts
@@ -1,2 +1,3 @@
 // 모든 공통 컴포넌트
 export * from "./Sidebar";
+export * from "./Accordion";
