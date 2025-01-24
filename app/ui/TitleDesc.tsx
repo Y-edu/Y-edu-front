697788a16504b21099bcba898fde16410d19a9aa
leehyewon0531 <ifpffk@naver.com>
2025-01-24 14:56:52 +0900

    feat: TitleDesc props + 스타일 추가

--- app/ui/TitleDesc.tsx
+++ app/ui/TitleDesc.tsx
@@ -1,5 +1,19 @@
-function TitleDesc() {
-  return <div></div>;
+interface TitleDescProps {
+  title: string;
+  desc: string;
+  maxWidth: string;
+  // maxHeight
+}
+
+function TitleDesc(props: TitleDescProps) {
+  const { title, desc, maxWidth } = props;
+
+  return (
+    <div className={`flex flex-col w-${maxWidth}`}>
+      <p className="text-titleColor">{title}</p>
+      <p className="text-descColor">{desc}</p>
+    </div>
+  );
 }
 
 export default TitleDesc;
