a2d211acb31d1de1b648010b6421d1ead90a3525
codekattt <widkid00@naver.com>
2025-01-25 03:23:43 +0900

    feat: usePatchTeacherModal Mutation 추가

---
+++ app/actions/patch-teacherModal/index.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+import { AxiosError } from "axios";
+
+import { httpService } from "../../utils/httpService";
+
+const teacherModalSchema = z.object({
+  data: z.object({
+    id: z.number(),
+    youtubeLink: z.string().optional(),
+    remark: z.string().optional(),
+  }),
+});
+
+type teacherModalResponse = z.infer<typeof teacherModalSchema>;
+
+export async function patchTeacherModal({
+  id,
+  youtubeLink,
+  remark,
+}: {
+  id: number;
+  youtubeLink?: string | undefined;
+  remark?: string | undefined;
+}) {
+  try {
+    const response = await httpService.patch<teacherModalResponse>(
+      `/api/teachers/${id}`,
+      {
+        youtubeLink,
+        remark,
+      },
+    );
+    const parseResult = teacherModalSchema.safeParse(response.data);
+
+    if (!parseResult.success) {
+      throw new Error("서버 데이터 형식과 일치하지 않습니다.");
+    }
+
+    return parseResult.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      throw new Error(`Axios Error: ${error.message}`);
+    } else {
+      throw error;
+    }
+  }
+}
