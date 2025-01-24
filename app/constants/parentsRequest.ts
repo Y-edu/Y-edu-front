f6ca0820c3b58a375ae7522760fda4553f813109
leehyewon0531 <ifpffk@naver.com>
2025-01-24 18:01:17 +0900

    feat: 학부모 요청 컴포넌트 제목 상수 정의

---
+++ app/constants/parentsRequest.ts
@@ -0,0 +1,12 @@
+export const PARENTS_REQUEST_TITLE = {
+  classTime: "수업 시수",
+  childAge: "아이 나이",
+  classType: "과외 유형",
+  teacherGender: "선생님 성별",
+  address: "주소",
+  classPurpose: "과외 목적",
+  childLevel: "아이 레벨",
+  teacherCondition: "선생님 조건",
+  teacherStyle: "선생님 스타일",
+  directivity: "원하는 수업 방향",
+};
