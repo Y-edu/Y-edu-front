31996f1eaa3d339e4b600952ad46cbc75a571bff
leehyewon0531 <ifpffk@naver.com>
2025-01-24 15:05:34 +0900

    feat: Accordion props + 스타일 추가

--- app/ui/Accordion.tsx
+++ app/ui/Accordion.tsx
@@ -1,12 +1,19 @@
 "use client";
 import { useState } from "react";
 
-function Accordion() {
+interface AccordionProps {
+  visibleContent: React.ReactNode;
+  hiddenContent: React.ReactNode;
+}
+
+function Accordion(props: AccordionProps) {
+  const { visibleContent, hiddenContent } = props;
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="relative w-11/12 rounded-3xl border border-gray-300 bg-white">
-      <div className="relative min-h-24 w-full">
+      <div className="relative min-h-24 w-full p-4">
+        <div className="w-11/12">{visibleContent}</div>
         <button
           onClick={() => {
             setIsOpen(!isOpen);
@@ -16,7 +23,11 @@ function Accordion() {
           {isOpen ? "접기" : "펼치기"}
         </button>
       </div>
-      <div className={`${isOpen ? "h-full" : "h-0"}`}></div>
+      <div
+        className={`${isOpen ? "h-full" : "h-0"} flex flex-col gap-4 overflow-hidden px-4`}
+      >
+        {hiddenContent}
+      </div>
     </div>
   );
 }
