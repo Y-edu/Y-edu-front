73ca9ffa4b6ce9ba6aafaf13294307f6421ebd5a
codekattt <widkid00@naver.com>
2025-01-27 17:23:20 +0900

    feat:학부모 리스트 마크업 구현

---
+++ app/types/ParentsList.ts
@@ -0,0 +1,13 @@
+export interface ParentsList {
+  id: number;
+  kakaoName: string;
+  classTime: string;
+  subject: string[];
+  monthlyFee: number;
+  source: string;
+  submittedAt: string;
+  acceptCount: number;
+  alertCount: number;
+  phoneNumber: string;
+  isDone: boolean;
+}
