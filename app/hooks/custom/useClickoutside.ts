fabc71f9f84fce70bebb996047cb5d37cdb6dcb7
leehyewon0531 <ifpffk@naver.com>
2025-01-28 11:31:37 +0900

    fix: useClickoutside 타입 수정

--- app/hooks/custom/useClickoutside.ts
+++ app/hooks/custom/useClickoutside.ts
@@ -1,11 +1,9 @@
 /* eslint-disable promise/prefer-await-to-callbacks */
 import { useEffect, MutableRefObject } from "react";
 
-export function useClickoutside<T extends MutableRefObject<HTMLDivElement>>(
-  ref: T,
-  callback?: () => void,
-  deps?: [],
-) {
+export function useClickoutside<
+  T extends MutableRefObject<HTMLDivElement | null>,
+>(ref: T, callback?: () => void, deps?: []) {
   useEffect(() => {
     const listener = (event: MouseEvent) => {
       if (!ref || !ref.current || ref.current.contains(event.target as Node)) {
