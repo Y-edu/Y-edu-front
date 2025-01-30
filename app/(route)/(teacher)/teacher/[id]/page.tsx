406982cd601e06e5a49f4aeda7becd1066546bd3
leehyewon0531 <ifpffk@naver.com>
2025-01-30 14:50:39 +0900

    feat: IconTitleChip 구현

--- app/(route)/(teacher)/teacher/[id]/page.tsx
+++ app/(route)/(teacher)/teacher/[id]/page.tsx
@@ -1,4 +1,9 @@
+import IconTitleChip from "../../../../components/teacher/IconTitleChip";
 import ProfileInfoBox from "../../../../components/teacher/ProfileInfoBox";
+import {
+  TEACHER_STYLE_ICON,
+  TEACHER_STYLE_TEXT,
+} from "../../../../constants/teacherStyle";
 
 export default function TeacherPage({ params }: { params: { id: string } }) {
   return (
@@ -10,9 +15,12 @@ export default function TeacherPage({ params }: { params: { id: string } }) {
             예시텍스트입니다. 예시텍스트입니다. 예시텍스트입니다.
             예시텍스트입니다. 예시텍스트입니다. 예시텍스트입니다.
             예시텍스트입니다. 예시텍스트입니다. 예시텍스트입니다.
-            예시텍스트입니다.
           </div>
         }
+      />
+      <IconTitleChip
+        title={`${TEACHER_STYLE_TEXT.passionate} 선생님`}
+        icon={TEACHER_STYLE_ICON.passionate}
       />
     </div>
   );
