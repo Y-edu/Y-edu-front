a7fe8b43416199082ca0fa2ac285b0fb9627885b
kimhyojung <706shin1728@naver.com>
2025-01-31 13:17:31 +0900

    chore: 목업 API 추가

--- __mocks__/handler.ts
+++ __mocks__/handler.ts
@@ -3,11 +3,13 @@ import { HttpResponse, http } from "msw";
 import { alimtalkhandlers } from "./alimtalk";
 import { parentRequestHandler } from "./parentsRequest";
 import { teacherListHandlers } from "./teacherList";
+import { matchingHandlers } from "./matching";
 
 export const handlers = [
   ...alimtalkhandlers,
   parentRequestHandler,
   ...teacherListHandlers,
+  ...matchingHandlers,
   // 테스트용 모든 핸들러 정의
   http.get("http://localhost:3000/api/users", () => {
     return HttpResponse.json({
---
+++ __mocks__/matching.ts
@@ -0,0 +1,39 @@
+import { HttpResponse, http } from "msw";
+
+export const matchingHandlers: ReturnType<typeof http.get>[] = [
+  http.get("http://localhost:3000/api/teacher/:id/matching/:matchingId", () => {
+    return HttpResponse.json({
+      data: {
+        online: false,
+        subject: "ENGLISH",
+        district: "서대문구",
+        dong: "홍은동",
+        detail: "초롱초롱 아파트 101동 101호",
+        classCount: 3,
+        classTime: 100,
+        goal: "",
+        wantTime: "화,목 오후 6시 이후",
+        wantDirection:
+          "고려대학교에서 사회학을 공부하고 있고 학원, 개인과외, 영어캠프 등 다양한 경로로 아이들을 가르쳐왔습니다. 학생들이 즐겁게 배울 수 있도록 최선을 다하고 있으며, 각자의 학습 스타일에 맞춘 지도를 중요하게 생각합니다.고려대학교에서 사회학을 공부하고 있고 학원, 개인과외, 영어캠프 등 다양한 경로로 아이들을 가르쳐왔습니다. 학생들이 즐겁게 배울 수 있도록 최선을 다하고 있으며, 각자의 학습 스타일에 맞춘 지도를 중요하게 생각합니다.고려대학교에서 사회학을 공부하고 있고 학원, 개인과외, 영어캠프 등 다양한 경로로 아이들을 가르쳐왔습니다. 학생들이 즐겁게 배울 수 있도록 최선을 다하고 있으며, 각자의 학습 스타일에 맞춘 지도를 중요하게 생각합니다.고려대학교에서 사회학을 공부하고 있고 학원, 개인과외, 영어캠프 등 다양한 경로로 아이들을 가르쳐왔습니다. 학생들이 즐겁게 배울 수 있도록 최선을 다하고 있으며, 각자의 학습 스타일에 맞춘 지도를 중요하게 생각합니다.",
+        favoriteCondition:
+          "고려대학교에서 사회학을 공부하고 있고 학원, 개인과외, 영어캠프 등 다양한 경로로 아이들을 가르쳐왔습니다. 학생들이 즐겁게 배울 수 있도록 최선을 다하고 있으며, 각자의 학습 스타일에 맞춘 지도를 중요하게 생각합니다.",
+      },
+    });
+  }),
+  http.post(
+    "http://localhost:3000/api/teacher/:id/matching/:matchingId/accept",
+    () => {
+      return HttpResponse.json({
+        data: "수락되었습니다",
+      });
+    },
+  ),
+  http.post(
+    "http://localhost:3000/api/teacher/:id/matching/:matchingId/accept",
+    () => {
+      return HttpResponse.json({
+        data: "거절되었습니다.",
+      });
+    },
+  ),
+];
