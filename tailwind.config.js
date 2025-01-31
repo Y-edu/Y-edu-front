c747e637ae8c62c6a84c7f68989727aa6f06553b
leehyewon0531 <ifpffk@naver.com>
2025-01-31 15:45:04 +0900

    style: 공통 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ const config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         primary: "#527DFF",
+        primaryLight: "#F3F7FF",
+        labelStrong: "#191F28",
         headColor: "#333B69",
         titleColor: "#232323",
         descColor: "#718EBF",
