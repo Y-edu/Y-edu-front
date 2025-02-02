5b55a1788d541d20b82a2cf0f7635050513fe572
leehyewon0531 <ifpffk@naver.com>
2025-02-02 20:36:12 +0900

    fix: baseURL 환경변수로 변경

--- app/utils/httpService.ts
+++ app/utils/httpService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000" // 개발 모드
+    ? `${process.env.NEXT_PUBLIC_SERVER_URL}:${process.env.NEXT_PUBLIC_PORT}` // 개발 모드
     : "https://2658d74c-2d23-41c2-8e9e-010551ee3f7b.mock.pstmn.io"; // 배포 모드
 
 export const httpService = axios.create({
