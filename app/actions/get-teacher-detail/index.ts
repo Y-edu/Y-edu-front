1ee1b669c7b1a10dadaa1a6cd593f01b6f0b070c
leehyewon0531 <ifpffk@naver.com>
2025-02-02 20:38:35 +0900

    fix: 선생님 상세 페이지 api 호출 함수 response 타입 보장

--- app/actions/get-teacher-detail/index.ts
+++ app/actions/get-teacher-detail/index.ts
@@ -1,71 +1,89 @@
 import { httpService } from "../../utils/httpService";
 
 export type DayOfWeek = "월" | "화" | "수" | "목" | "금" | "토" | "일";
+export type SubjectType = "english" | "math";
 
 export interface TeacherSimpleDetailsParams {
   teacherId: string;
 }
 
 export interface TeacherDetailsParams extends TeacherSimpleDetailsParams {
-  subject: "english" | "math";
+  subject: SubjectType;
 }
 
 export interface TeacherDetailsTeacherResponse {
-  appealPoints: Array<string>;
-  comment: string;
-  introduce: string;
-  teachingHistory: number;
-  teachingExperiences: Array<string>;
-  university: string;
-  major: string;
-  highSchool: string;
-  teachingStyle1: string;
-  teachingStyleInfo1: string;
-  teachingStyle2: string;
-  teachingStyleInfo2: string;
-  recommendStudents: Array<string>;
+  data: {
+    appealPoints: Array<string>;
+    comment: string;
+    introduce: string;
+    teachingHistory: number;
+    teachingExperiences: Array<string>;
+    foreignExperiences?: Array<string>;
+    university: string;
+    major: string;
+    highSchool: string;
+    teachingStyle1: string;
+    teachingStyleInfo1: string;
+    teachingStyle2: string;
+    teachingStyleInfo2: string;
+    recommendStudents: Array<string>;
+  };
 }
 
 export interface TeacherDetailsClassResponse {
-  teachingStyle: string;
-  managementStyle: string;
-}
-
-export interface TeacherDetailsInfoResponse {
-  profile: string;
-  nickName: string;
+  data: {
+    teachingStyle: string;
+    managementStyle: string;
+  };
 }
 
 export interface TeacherDetailsAvailableResponse {
-  districts: Array<string>;
-  availableTimes: {
-    [key in DayOfWeek]: Array<string>;
+  data: {
+    districts: Array<string>;
+    availableTimes: {
+      [key in DayOfWeek]: Array<string>;
+    };
+  };
+}
+
+export interface TeacherDetailsInfoResponse {
+  data: {
+    profile: string;
+    nickName: string;
   };
 }
 
 /** '선생님' 탭 정보 */
-export function getTeacherDetailsTeacher(params: TeacherDetailsParams) {
+export function getTeacherDetailsTeacher(
+  params: TeacherDetailsParams,
+): Promise<TeacherDetailsTeacherResponse> {
   const { teacherId, subject } = params;
 
   return httpService.get(`/teacher/details/${subject}/teacher/${teacherId}`);
 }
 
 /** '수업' 탭 정보 */
-export function getTeacherDetailsClass(params: TeacherDetailsParams) {
+export function getTeacherDetailsClass(
+  params: TeacherDetailsParams,
+): Promise<TeacherDetailsClassResponse> {
   const { teacherId, subject } = params;
 
   return httpService.get(`/teacher/details/${subject}/class/${teacherId}`);
 }
 
 /** '지역/시간' 탭 정보 */
-export function getTeacherDetailsAvailable(params: TeacherSimpleDetailsParams) {
+export function getTeacherDetailsAvailable(
+  params: TeacherSimpleDetailsParams,
+): Promise<TeacherDetailsAvailableResponse> {
   const { teacherId } = params;
 
   return httpService.get(`/teacher/details/available/${teacherId}`);
 }
 
 /** 프로필 사진 및 닉네임 정보 */
-export function getTeacherDetailsInfo(params: TeacherSimpleDetailsParams) {
+export function getTeacherDetailsInfo(
+  params: TeacherSimpleDetailsParams,
+): Promise<TeacherDetailsInfoResponse> {
   const { teacherId } = params;
 
   return httpService.get(`/teacher/details/info/${teacherId}`);
