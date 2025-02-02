0a30e61908be268cd864548b66456dccf5e5b424
leehyewon0531 <ifpffk@naver.com>
2025-02-02 18:02:15 +0900

    feat: 선생님 상세 페이지 요청/응답 타입 정의

---
+++ app/actions/get-teacher-detail/index.ts
@@ -0,0 +1,44 @@
+export type DayOfWeek = "월" | "화" | "수" | "목" | "금" | "토" | "일";
+
+export interface TeacherSimpleDetailsParams {
+  teacherId: string;
+}
+
+export interface TeacherDetailsParams extends TeacherSimpleDetailsParams {
+  subject: "english" | "math";
+}
+
+export interface TeacherDetailsTeacherResponse {
+  appealPoints: Array<string>;
+  comment: string;
+  introduce: string;
+  teachingHistory: number;
+  teachingExperiences: Array<string>;
+  university: string;
+  major: string;
+  highSchool: string;
+  teachingStyle1: string;
+  teachingStyleInfo1: string;
+  teachingStyle2: string;
+  teachingStyleInfo2: string;
+  recommendStudents: Array<string>;
+}
+
+export interface TeacherDetailsClassResponse {
+  teachingStyle: string;
+  managementStyle: string;
+}
+
+export interface TeacherDetailsInfoResponse {
+  profile: string;
+  nickName: string;
+}
+
+export interface TeacherDetailsAvailableResponse {
+  districts: Array<string>;
+  avaialbeTimes: [
+    {
+      [key in DayOfWeek]: Array<string>;
+    },
+  ];
+}
