90b65d91ffd8a6405c18279f6db6c4318680859c
leehyewon0531 <ifpffk@naver.com>
2025-02-02 20:57:59 +0900

    fix: 지역/시간 탭 api 호출로 변경

--- app/components/teacher/TeacherDetail/TeacherDetailRegionTime.tsx
+++ app/components/teacher/TeacherDetail/TeacherDetailRegionTime.tsx
@@ -2,44 +2,64 @@
 import { useParams } from "next/navigation";
 import BulletList from "../../../ui/List/BulletList";
 import ProfileInfoBox from "../ProfileInfoBox";
+import { useGetTeacherDetailsAvailable } from "../../../hooks/query/useGetTeacherDetails";
+import { DayOfWeek } from "../../../actions/get-teacher-detail";
 
 export default function TeacherDetailRegionTime() {
   const params = useParams();
-  const teacherId = params.id;
+  const teacherId = Array.isArray(params.id) ? params.id[0] : params.id || "";
+  const { data } = useGetTeacherDetailsAvailable({ teacherId });
 
-  const regions = ["강남구", "서대문구", "비대면"];
-  const times = [
-    "월: 13시, 15시, 17시, 18시",
-    "화: 14시, 15시",
-    "수: 불가",
-    "목: 불가",
-    "금: 16시, 18시, 19시",
-    "토: 불가",
-    "일: 16시, 18시, 19시",
-  ];
+  /** '요일: 시간' 형태로 만드는 함수 */
+  function formatAvailableTimes(availableTimes: {
+    [key in DayOfWeek]: Array<string>;
+  }): string[] {
+    return Object.entries(availableTimes).map(([day, times]) => {
+      if (times.length === 0) {
+        return `${day}: 불가`;
+      }
+
+      const formattedTimes = times
+        .map((time) => {
+          const [hour, minute] = time.split(":");
+          const hourFormatted = parseInt(hour, 10);
+          const minuteFormatted =
+            minute === "00" ? "" : ` ${parseInt(minute, 10)}분`;
+          return `${hourFormatted}시${minuteFormatted}`;
+        })
+        .join(", ");
+
+      return `${day}: ${formattedTimes}`;
+    });
+  }
 
   return (
     <div className="flex flex-col gap-[10px] bg-primaryPale">
-      <ProfileInfoBox
-        title={
-          <p>
-            이 <span className="text-primaryNormal">지역</span>에서 과외가
-            가능해요.
-          </p>
-        }
-      >
-        <BulletList items={regions} />
-      </ProfileInfoBox>
-      <ProfileInfoBox
-        title={
-          <p>
-            선생님이 <span className="text-primaryNormal">선호하는 시간</span>
-            이에요!
-          </p>
-        }
-      >
-        <BulletList items={times} />
-      </ProfileInfoBox>
+      {data && (
+        <>
+          <ProfileInfoBox
+            title={
+              <p>
+                이 <span className="text-primaryNormal">지역</span>에서 과외가
+                가능해요.
+              </p>
+            }
+          >
+            <BulletList items={data.data.districts} />
+          </ProfileInfoBox>
+          <ProfileInfoBox
+            title={
+              <p>
+                선생님이{" "}
+                <span className="text-primaryNormal">선호하는 시간</span>
+                이에요!
+              </p>
+            }
+          >
+            <BulletList items={formatAvailableTimes(data.data.availables)} />
+          </ProfileInfoBox>
+        </>
+      )}
     </div>
   );
 }
