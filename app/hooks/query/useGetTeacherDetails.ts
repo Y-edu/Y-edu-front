33a0e032f56fcae9de7bdc60cab70419da825dfc
leehyewon0531 <ifpffk@naver.com>
2025-02-02 20:46:42 +0900

    feat: 선생님 상세 페이지 수업 탭 useQuery

--- app/hooks/query/useGetTeacherDetails.ts
+++ app/hooks/query/useGetTeacherDetails.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import {
   TeacherDetailsParams,
+  getTeacherDetailsClass,
   getTeacherDetailsTeacher,
 } from "../../actions/get-teacher-detail";
 
@@ -9,7 +10,16 @@ export function useGetTeacherDetailsTeacher(params: TeacherDetailsParams) {
     queryKey: ["teacher-details-teacher", params],
     queryFn: () => {
       const res = getTeacherDetailsTeacher(params);
+      return res;
+    },
+  });
+}
 
+export function useGetTeacherDetailsClass(params: TeacherDetailsParams) {
+  return useQuery({
+    queryKey: ["teacher-details-class", params],
+    queryFn: () => {
+      const res = getTeacherDetailsClass(params);
       return res;
     },
   });
