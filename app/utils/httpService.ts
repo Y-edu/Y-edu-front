d2ed1f58b53c0c27a837e145279eb0914c4af0bb
kimhyojung <706shin1728@naver.com>
2025-02-06 17:18:29 +0900

    refactor: httpServer base URL 수정

--- app/utils/httpService.ts
+++ app/utils/httpService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? `https://dev.yedu-develop.com:8181` // 개발 모드
+    ? `${process.env.NEXT_PUBLIC_SERVER_URL}:${process.env.NEXT_PUBLIC_PORT}` // 개발 모드
     : `${process.env.NEXT_PUBLIC_SERVER_URL}`; // 배포 모드
 
 export const httpService = axios.create({
