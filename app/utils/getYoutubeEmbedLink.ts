758ac8a1b95714892235fccf73457d8f5cf1b806
leehyewon0531 <ifpffk@naver.com>
2025-02-09 16:21:48 +0900

    feat: youtube 링크 embed 링크로 변환하는 함수 추가

---
+++ app/utils/getYoutubeEmbedLink.ts
@@ -0,0 +1,8 @@
+const getYoutubeEmbedLink = (url: string) => {
+  const videoIdMatch = url.match(
+    /(?:youtu\.be\/|youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\?v=))([^&]+)/,
+  );
+  return videoIdMatch
+    ? `https://www.youtube.com/embed/${videoIdMatch[1]}?autoplay=0`
+    : "";
+};
