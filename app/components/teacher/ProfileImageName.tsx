1a7f0052d966a03d6cb137c9490f2945eecfb400
leehyewon0531 <ifpffk@naver.com>
2025-02-09 16:21:28 +0900

    feat: ProfileImageName 사진 null일 때 처리 추가

--- app/components/teacher/ProfileImageName.tsx
+++ app/components/teacher/ProfileImageName.tsx
@@ -1,7 +1,7 @@
 import Image, { StaticImageData } from "next/image";
 
 interface ProfileImageNameProps {
-  imgSrc: StaticImageData | string;
+  imgSrc: StaticImageData | string | null;
   name: string;
 }
 
@@ -10,13 +10,17 @@ export default function ProfileImageName(props: ProfileImageNameProps) {
 
   return (
     <div className="flex w-[121px] flex-col items-center gap-4">
-      <Image
-        src={imgSrc}
-        alt="프로필 이미지"
-        width={110}
-        height={110}
-        className="h-auto w-[110px]"
-      />
+      {imgSrc ? (
+        <Image
+          src={imgSrc}
+          alt="프로필 이미지"
+          width={110}
+          height={110}
+          className="h-auto w-[110px]"
+        />
+      ) : (
+        <div className="h-[180px] w-[110px] rounded-xl bg-gray-200"></div>
+      )}
       <p className="font-pretendard text-xl font-bold text-labelStrong">
         {name}
       </p>
