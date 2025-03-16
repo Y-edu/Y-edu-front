3dcd5646c9f33968830eda0c1bfb2d08d8986d65
kimhyojung <706shin1728@naver.com>
2025-03-16 15:10:28 +0900

    refactor : 불가능한 날 처리

--- app/actions/put-teacher-avaiable/index.ts
+++ app/actions/put-teacher-avaiable/index.ts
@@ -9,10 +9,16 @@ export const putTeacherAvaiable = async ({
   phoneNumber: string;
   available: Record<string, string[]>;
 }) => {
+  for (const [key, value] of Object.entries(available)) {
+    if (value.length === 0) {
+      available[key] = ["불가"];
+    }
+  }
+
   const response = await httpService.put<string>(`/teacher/info/available`, {
     name,
     phoneNumber,
-    available,
+    available: [available],
   });
 
   return response.data;
