3b79e38650011d7b00a2dc9efe81145965cb72f6
codekattt <widkid00@naver.com>
2025-03-21 16:20:56 +0900

    add: 변경사항 저장하지 않고 뒤로가기 누를 때 경고 모달 추가

--- app/(route)/(teachersetting)/teachersetting/time/page.tsx
+++ app/(route)/(teachersetting)/teachersetting/time/page.tsx
@@ -2,14 +2,14 @@
 
 import { ErrorBoundary } from "react-error-boundary";
 
-import { SettingTeacherTime } from "@/components/teacher/TeacherSetting/TeacherSettingTime";
+import { TeacherSettingTime } from "@/components/teacher/TeacherSetting/TeacherSettingTime";
 import ErrorUI from "@/ui/ErrorUI";
 
 export default function TeacherTimeSetting() {
   return (
     <ErrorBoundary fallback={<ErrorUI />}>
       <div className="w-full">
-        <SettingTeacherTime />
+        <TeacherSettingTime />
       </div>
     </ErrorBoundary>
   );
