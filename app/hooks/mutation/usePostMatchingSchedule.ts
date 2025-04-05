01141fcc121ebe5d69c2a9a53739bd5051f28022
codekattt <widkid00@naver.com>
2025-04-06 03:22:03 +0900

    feat: 알림톡 테이블 매칭버튼 api 코드 작성

---
+++ app/hooks/mutation/usePostMatchingSchedule.ts
@@ -0,0 +1,10 @@
+// 알림톡 테이블에서 과외건과 선생님을 매칭하는 mutation 훅
+import { useMutation } from "@tanstack/react-query";
+
+import { postMatchingSchedule } from "@/actions/post-matching-schedule";
+
+export function usePostMatchingSchedule() {
+  return useMutation({
+    mutationFn: postMatchingSchedule,
+  });
+}
