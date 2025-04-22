40c4a7c9493ecd1ff3cafb22a8d024f4cd5e55d2
codekattt <widkid00@naver.com>
2025-04-22 20:58:07 +0900

    setting: 새파일 생성 시 절대경로 인식 못하는 문제 해결

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 const nextConfig = {
   webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "app"),
+    };
+
     if (process.env.NODE_ENV === "production") {
       config.module.rules?.push(
         {
