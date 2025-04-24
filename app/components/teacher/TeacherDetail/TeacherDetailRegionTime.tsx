d200a7eced6c6998666ec7ba07046d5d0a6f17e0
leehyewon0531 <ifpffk@naver.com>
2025-04-24 11:11:08 +0900

    fix: 일괄정보 불러오고 props로 내려주게 수정

--- app/components/teacher/TeacherDetail/TeacherDetailRegionTime.tsx
+++ app/components/teacher/TeacherDetail/TeacherDetailRegionTime.tsx
@@ -1,56 +1,56 @@
 "use client";
-import { useParams } from "next/navigation";
 
-import { useGetTeacherDetailsAvailable } from "@/hooks/query/useGetTeacherDetails";
 import BulletList from "@/ui/List/BulletList";
 import { formatAvailableTimes } from "@/utils/formatAvailableTimes";
+import { DayOfWeek } from "@/actions/get-teacher-detail";
 
 import ProfileInfoBox from "app/components/teacher/ProfileInfoBox";
 
-export default function TeacherDetailRegionTime() {
-  const params = useParams();
-  const teacherId = Array.isArray(params.id) ? params.id[0] : params.id || "";
+interface TeacherDetailRegionTimeProps {
+  districts: Array<string>;
+  available: {
+    [key in DayOfWeek]: Array<string>;
+  };
+}
 
-  const { data, error } = useGetTeacherDetailsAvailable({ teacherId });
-  if (error) throw error;
+export default function TeacherDetailRegionTime(
+  props: TeacherDetailRegionTimeProps,
+) {
+  const { districts, available } = props;
 
   return (
     <div className="flex flex-col gap-[10px] bg-primaryPale">
-      {data && (
-        <>
+      <ProfileInfoBox
+        title={
+          <p>
+            이 <span className="text-primaryNormal">지역</span>에서 과외가
+            가능해요.
+          </p>
+        }
+      >
+        <BulletList items={districts} />
+      </ProfileInfoBox>
+      {available &&
+        !Object.values(available).every(
+          (times) => times.length === 1 && times[0] === "불가",
+        ) && (
           <ProfileInfoBox
             title={
-              <p>
-                이 <span className="text-primaryNormal">지역</span>에서 과외가
-                가능해요.
-              </p>
+              <div className="flex flex-col gap-1">
+                <p>
+                  선생님이{" "}
+                  <span className="text-primaryNormal">선호하는 시간</span>
+                  이에요!
+                </p>
+                <p className="text-[15px] font-medium leading-[152%] text-labelAssistive">
+                  선생님과 협의하여 시간을 조율할 수 있어요.
+                </p>
+              </div>
             }
           >
-            <BulletList items={data.districts} />
+            <BulletList items={formatAvailableTimes(available)} />
           </ProfileInfoBox>
-          {data.availables &&
-            !Object.values(data.availables).every(
-              (times) => times.length === 1 && times[0] === "불가",
-            ) && (
-              <ProfileInfoBox
-                title={
-                  <div className="flex flex-col gap-1">
-                    <p>
-                      선생님이{" "}
-                      <span className="text-primaryNormal">선호하는 시간</span>
-                      이에요!
-                    </p>
-                    <p className="text-[15px] font-medium leading-[152%] text-labelAssistive">
-                      선생님과 협의하여 시간을 조율할 수 있어요.
-                    </p>
-                  </div>
-                }
-              >
-                <BulletList items={formatAvailableTimes(data.availables)} />
-              </ProfileInfoBox>
-            )}
-        </>
-      )}
+        )}
     </div>
   );
 }
