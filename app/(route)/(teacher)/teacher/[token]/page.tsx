d240fe6c9fd185634dc8011ccdffcc16f169ed42
leehyewon0531 <ifpffk@naver.com>
2025-04-24 11:11:29 +0900

    feat: token url로 선생님 상세 정보 페이지 생성

---
+++ app/(route)/(teacher)/teacher/[token]/page.tsx
@@ -0,0 +1,68 @@
+"use client"; // client component 분리는 리팩토링때 진행하겠습니다 😅
+import { ErrorBoundary } from "react-error-boundary";
+
+import TeacherDetailRegionTime from "@/components/teacher/TeacherDetail/TeacherDetailRegionTime";
+import ProfileTop from "@/components/teacher/ProfileTop";
+import TeacherDetailClass from "@/components/teacher/TeacherDetail/TeacherDetailClass";
+import TeacherDetailMain from "@/components/teacher/TeacherDetail/TeacherDetailMain";
+import TabBar from "@/ui/Bar/TabBar";
+import ErrorUI from "@/ui/ErrorUI";
+import { useGetTeacherAllDetails } from "@/hooks/query/useGetTeacherAllDetails";
+
+export default function TeacherPage({ params }: { params: { token: string } }) {
+  const { token } = params;
+  const { data, error } = useGetTeacherAllDetails({ token });
+
+  if (error) throw error;
+
+  return (
+    <ErrorBoundary fallback={<ErrorUI />}>
+      {data && (
+        <div className="w-full">
+          <ProfileTop profile={data.profile} nickName={data.nickName} />
+          <TabBar
+            tabs={[
+              {
+                trigger: "선생님",
+                content: (
+                  <TeacherDetailMain
+                    comment={data.comment}
+                    introduce={data.introduce}
+                    teachingHistory={data.teachingHistory}
+                    teachingExperiences={data.teachingExperiences}
+                    foreignExperiences={data.foreignExperiences}
+                    university={data.university}
+                    major={data.major}
+                    highSchool={data.highSchool}
+                    teachingStyle1={data.teachingStyle1}
+                    teachingStyleInfo1={data.teachingStyleInfo1}
+                    teachingStyle2={data.teachingStyle2}
+                    teachingStyleInfo2={data.teachingStyleInfo2}
+                  />
+                ),
+              },
+              {
+                trigger: "수업",
+                content: (
+                  <TeacherDetailClass
+                    teachingStyle={data.teachingStyle}
+                    video={data.video}
+                  />
+                ),
+              },
+              {
+                trigger: "지역/시간",
+                content: (
+                  <TeacherDetailRegionTime
+                    districts={data.districts}
+                    available={data.available}
+                  />
+                ),
+              },
+            ]}
+          />
+        </div>
+      )}
+    </ErrorBoundary>
+  );
+}
