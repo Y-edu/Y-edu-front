8deaa6db67b65cfe6d5be0e23e0942ad217e277e
codekattt <widkid00@naver.com>
2025-04-25 01:07:27 +0900

    feat: 매칭 수락/거절 시 선생님 시간 최신화 시키는 타임테이블 페이지 코드 작업

--- app/(route)/(teacher)/teacher/notify/[token]/select-time/page.tsx
+++ app/(route)/(teacher)/teacher/notify/[token]/select-time/page.tsx
@@ -1,3 +1,43 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+
+import { TeacherSettingTime } from "@/components/teacher/TeacherSetting/TeacherSettingTime";
+import { usePostTutoringAccept } from "@/hooks/mutation/usePostTutoringAccept";
+
 export default function TeacherClassMatchingSelectTimePage() {
-  return <div>타임테이블 페이지입니다.</div>;
+  const params = useParams();
+
+  if (!params.token || Array.isArray(params.token)) {
+    throw new Error("유효하지 않은 토큰입니다.");
+  }
+  const token = params.token;
+
+  const router = useRouter();
+  const { mutate } = usePostTutoringAccept();
+  const handleMatch = () => {
+    mutate(
+      { token },
+      {
+        onSuccess: () => {
+          router.push(`/teacher/notify/${token}/complete`);
+        },
+      },
+    );
+  };
+
+  return (
+    <TeacherSettingTime
+      submitLabel="매칭 신청하기"
+      onSubmit={handleMatch}
+      onBackButtonConfirm={() => window.history.back()}
+      onPopstateConfirm={() => window.history.back()}
+      disableUnsavedWarning
+      isConfirm={{
+        title: "매칭 신청 확인",
+        message: "정말 이 시간으로 매칭 신청을 진행하시겠습니까?",
+        confirmText: "신청하기",
+        cancelText: "취소",
+      }}
+    />
+  );
 }
