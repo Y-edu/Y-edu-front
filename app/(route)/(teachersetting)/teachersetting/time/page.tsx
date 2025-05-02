074b48ccee6a5f27bc6c3c0e0dca7138907f00d3
leehyewon0531 <ifpffk@naver.com>
2025-05-02 14:19:40 +0900

    feat: initialTime 있으면 넣어주기

--- app/(route)/(teachersetting)/teachersetting/time/page.tsx
+++ app/(route)/(teachersetting)/teachersetting/time/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { ErrorBoundary } from "react-error-boundary";
+import { useSearchParams } from "next/navigation";
 
 import { TeacherSettingTime } from "@/components/teacher/TeacherSetting/TeacherSettingTime";
 import ErrorUI from "@/ui/ErrorUI";
+import { useGetTeacherSettingTimeByToken } from "@/hooks/query/useGetTeacherSettingTimeByToken";
 
 export default function TeacherTimeSetting() {
+  const token = useSearchParams().get("token");
+  const { data } = useGetTeacherSettingTimeByToken({
+    token: token || "",
+  });
+
   return (
     <ErrorBoundary fallback={<ErrorUI />}>
       <div className="w-full">
-        <TeacherSettingTime />
+        <TeacherSettingTime initialAvailable={data?.available} />
       </div>
     </ErrorBoundary>
   );
