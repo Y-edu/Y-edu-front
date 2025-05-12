ab46709ff400d26d0d2408e1a5b4e8256559c129
leehyewon0531 <ifpffk@naver.com>
2025-05-12 23:09:22 +0900

    feat: get-schedules action 함수 구현

---
+++ app/actions/get-schedules/index.ts
@@ -0,0 +1,20 @@
+import { httpService } from "@/utils/httpService";
+
+import { DayOfWeek } from "../get-teacher-detail";
+
+export interface ClassTimeDTO {
+  start: string;
+  classTime: number;
+}
+
+export interface SchedulesResponse {
+  schedules: Partial<Record<DayOfWeek, ClassTimeDTO[]>>;
+}
+
+export async function getSchedules({ token }: { token: string }) {
+  const res = await httpService.get<SchedulesResponse>(
+    `/schedules?token=${token}`,
+  );
+
+  return res.data;
+}
