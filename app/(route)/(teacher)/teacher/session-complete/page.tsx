5604edb1553be50dc93984a5716c7f401a534b7c
leehyewon0531 <ifpffk@naver.com>
2025-05-14 17:04:41 +0900

    feat: session-complete 일정 없을 때 뷰 구현

--- app/(route)/(teacher)/teacher/session-complete/page.tsx
+++ app/(route)/(teacher)/teacher/session-complete/page.tsx
@@ -1,17 +1,60 @@
 "use client";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { CircularProgress } from "@mui/material";
 
-import Image from "next/image";
-import Calender from "public/images/calendar.svg";
-
-import Button from "@/ui/Button";
+import { useGetSchedules } from "@/hooks/query/useGetSchedules";
+import SessionSchedule from "@/components/teacher/Session/SessionSchedule";
 
 export default function SessionCompletePage() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const { data, isLoading } = useGetSchedules({ token: token ?? "" });
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(data?.schedules ?? {}).length === 0) setIsEmpty(true);
+    else setIsEmpty(false);
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <CircularProgress />
+      </div>
+    );
+  }
+
   return (
-    <Button
-      leftIcon={<Image src={Calender} width={20} height={20} alt="calender" />}
-      className="text-grey-700 w-fit justify-normal gap-1 bg-transparent px-3 py-[6px] text-sm"
-    >
-      전체 일정 변경
-    </Button>
+    <div>
+      {/* 일정 비어 있으면 일정 설정 페이지, 아니면 일정 확인 페이지 */}
+      {!isEmpty ? (
+        <div className="flex flex-col items-center">
+          <div className="mb-8 flex h-12 w-full items-center justify-center text-lg text-grey-900">
+            과외 식별자
+          </div>
+          <SessionSchedule
+            className="mb-24"
+            title={`Y-edu 에게 진행 중인\n과외 일정을 알려주세요`}
+            token={token ?? ""}
+          />
+        </div>
+      ) : (
+        <div>
+          {Object.entries(data?.schedules ?? {}).map(([day, times]) => (
+            <div key={day}>
+              <h3>{day}</h3>
+              <ul>
+                {times.map((time, idx) => (
+                  <li key={idx}>
+                    시작 시간: {time.start}, 수업 시간: {time.classMinute}분
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
