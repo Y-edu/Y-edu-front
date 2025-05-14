7c95e206d0f5b362472465bf48425549e36f07a0
leehyewon0531 <ifpffk@naver.com>
2025-05-14 23:04:01 +0900

    feat: 과외 완료 UI 구현

--- app/(route)/(teacher)/teacher/session-complete/page.tsx
+++ app/(route)/(teacher)/teacher/session-complete/page.tsx
@@ -6,6 +6,11 @@ import { CircularProgress } from "@mui/material";
 import { useGetSchedules } from "@/hooks/query/useGetSchedules";
 import SessionSchedule from "@/components/teacher/Session/SessionSchedule";
 import HeaderWithBack from "@/components/result/HeaderWithBack";
+import TitleSection from "@/ui/TitleSection";
+import DivWithLabel from "@/components/result/DivWIthLabel";
+import Radio from "@/ui/Radio";
+import { HOMEWORK_PROGRESS_LIST } from "@/constants/session/homework";
+import Textarea from "@/ui/Textarea";
 
 export default function SessionCompletePage() {
   const searchParams = useSearchParams();
@@ -27,7 +32,7 @@ export default function SessionCompletePage() {
   }
 
   return (
-    <div>
+    <div className="w-full">
       {/* 일정 비어 있으면 일정 설정 페이지, 아니면 과외 완료 페이지 */}
       {isEmpty ? (
         <div className="flex flex-col items-center">
@@ -43,7 +48,52 @@ export default function SessionCompletePage() {
           </HeaderWithBack>
         </div>
       ) : (
-        <div>과외 완료 기능 페이지</div>
+        <div className="flex w-full flex-col items-center">
+          <HeaderWithBack
+            title="수업 리뷰"
+            hasBack
+            mainClassName="pt-8 w-full px-5"
+          >
+            <div className="flex w-full flex-col">
+              <div className="mb-5 flex flex-col gap-10">
+                <TitleSection>
+                  <TitleSection.Title>
+                    수업을 간략히 리뷰해주세요
+                  </TitleSection.Title>
+                  <TitleSection.Description>
+                    작성하신 내용은 학부모님께 전달됩니다
+                  </TitleSection.Description>
+                </TitleSection>
+                <DivWithLabel
+                  label="아이가 숙제를 모두 완료했나요?"
+                  className="w-full"
+                >
+                  {HOMEWORK_PROGRESS_LIST.map((item) => (
+                    <div key={item.value} className="py-4">
+                      <Radio
+                        label={item.label}
+                        selected={false}
+                        onClick={() => {}}
+                        {...(item.value !== 0 && {
+                          subLabel: `${item.value}%`,
+                        })}
+                      />
+                    </div>
+                  ))}
+                </DivWithLabel>
+              </div>
+              {/* 아래는 그냥 divider */}
+              <div className="-mx-5 border-t-8 border-gray-100" />
+              <DivWithLabel label="아이의 이해도" className="mb-40 mt-5">
+                <Textarea
+                  value=""
+                  onChange={() => {}}
+                  placeholder={`예) 'I go to bed at 9'처럼 일상 표현은 금방 따라왔지만, 의문문으로 바꾸는 건 어려워했어요.`}
+                />
+              </DivWithLabel>
+            </div>
+          </HeaderWithBack>
+        </div>
       )}
     </div>
   );
