660cfd621d7f0f5f6d1282b48f08eae8ba9ac1c8
codekattt <widkid00@naver.com>
2025-05-16 19:34:12 +0900

    fix: mutation 후 바로 캐시 최신화하도록 코드 수정

--- app/hooks/mutation/usePutSchedules.ts
+++ app/hooks/mutation/usePutSchedules.ts
@@ -1,10 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { putSchedules } from "@/actions/put-schedules";
 
 export function usePutSchedules() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: putSchedules,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["schedules"] });
+    },
     onError: (error: unknown) => {
       if (error instanceof Error) {
         alert("스케줄 등록 중 문제가 발생했어요. 다시 시도해주세요.");
