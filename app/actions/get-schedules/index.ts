b1d5c01a2f35ca3045081d6894be9c91d40b68ee
codekattt <widkid00@naver.com>
2025-05-16 17:10:11 +0900

    fix: api 스펙 변경에 따른 코드 수정

--- app/actions/get-schedules/index.ts
+++ app/actions/get-schedules/index.ts
@@ -6,10 +6,14 @@ export interface ClassTimeDTO {
   classMinute: number;
 }
 
-export interface SchedulesResponse {
+export interface ScheduleItem {
   applicationFormId: string;
+  classMatchingId: number;
+  send: boolean;
   schedules: Partial<Record<DayOfWeek, ClassTimeDTO[]>>;
 }
+
+export type SchedulesResponse = ScheduleItem[];
 
 export async function getSchedules({ token }: { token: string }) {
   const res = await httpService.get<SchedulesResponse>(
