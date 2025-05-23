6db613cd8d116616ca2033745d7cee76588f3828
codekattt <widkid00@naver.com>
2025-05-23 16:43:32 +0900

    fix: 탭 누를 때 자동으로 가로 스크롤되게 코드 수정

--- app/ui/Bar/TabBar.tsx
+++ app/ui/Bar/TabBar.tsx
@@ -21,8 +21,8 @@ export default function TabBar({
   tabs,
   scrollMode = false,
   paramKey,
-  listClassName,
-  buttonClassName,
+  listClassName = "",
+  buttonClassName = "",
 }: TabBarProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -31,14 +31,14 @@ export default function TabBar({
     : tabs[0].trigger;
   const [selectedTab, setSelectedTab] = useState(initial);
   const sectionRefs = useRef<Record<string, HTMLDivElement | null>>({}).current;
+  const buttonRefs = useRef<Array<HTMLButtonElement | null>>([]);
 
-  const handleTabClick = (trigger: string) => {
+  const handleTabClick = (trigger: string, idx: number) => {
     setSelectedTab(trigger);
     if (paramKey) {
       const params = new URLSearchParams(searchParams.toString());
       params.set(paramKey, trigger);
       router.push(`${window.location.pathname}?${params.toString()}`);
-      return;
     }
     if (scrollMode && sectionRefs[trigger]) {
       sectionRefs[trigger].scrollIntoView({
@@ -46,6 +46,11 @@ export default function TabBar({
         block: "start",
       });
     }
+    buttonRefs.current[idx]?.scrollIntoView({
+      behavior: "smooth",
+      inline: "start",
+      block: "nearest",
+    });
   };
 
   useEffect(() => {
@@ -80,16 +85,19 @@ export default function TabBar({
           listClassName,
         )}
       >
-        {tabs.map((tab) => (
+        {tabs.map((tab, idx) => (
           <button
             key={tab.trigger}
+            ref={(el: HTMLButtonElement | null): void => {
+              buttonRefs.current[idx] = el;
+            }}
             className={cn(
               "flex-1 border-b-2 border-transparent py-[10px] text-center font-[500] leading-[146%] tracking-[-0.02em] text-grey-500",
               selectedTab === tab.trigger &&
                 "border-primaryNormal font-bold text-primaryNormal",
               buttonClassName,
             )}
-            onClick={() => handleTabClick(tab.trigger)}
+            onClick={() => handleTabClick(tab.trigger, idx)}
           >
             {tab.trigger}
           </button>
