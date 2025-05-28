3b9c9fe192bf21d82cdeb530b665af676290d136
leehyewon0531 <ifpffk@naver.com>
2025-05-28 16:49:45 +0900

    feat: 수업관리 페이지들 헤더 추가

--- app/(route)/(admin)/zuzuclubadmin/class-management/[formId]/page.tsx
+++ app/(route)/(admin)/zuzuclubadmin/class-management/[formId]/page.tsx
@@ -1,3 +1,16 @@
-export default function ClassManagementDetailPage() {
-  return <div>상세정보 예시</div>;
+export default function ClassManagementDetailPage({
+  params,
+}: {
+  params: { formId: string };
+}) {
+  const { formId } = params;
+  const decodedFormId = decodeURIComponent(formId); // 한글 문자열 디코딩
+
+  return (
+    <div>
+      <h1 className="flex h-[100px] w-full items-center bg-white pl-16 text-[22px] font-bold text-gray-700">
+        {decodedFormId}
+      </h1>
+    </div>
+  );
 }
--- app/(route)/(admin)/zuzuclubadmin/class-management/page.tsx
+++ app/(route)/(admin)/zuzuclubadmin/class-management/page.tsx
@@ -2,8 +2,13 @@ import ClassList from "@/components/admin/ClassList";
 
 export default function ClassManagementHome() {
   return (
-    <div className="p-4 pt-2">
-      <ClassList />
+    <div>
+      <h1 className="flex h-[100px] w-full items-center bg-white pl-16 text-[22px] font-bold text-gray-700">
+        수업관리
+      </h1>
+      <div className="p-4 pt-2">
+        <ClassList />
+      </div>
     </div>
   );
 }
