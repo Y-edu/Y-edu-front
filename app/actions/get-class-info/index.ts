f3a96b873679d70b2794d0420c84bc51baf6bf64
leehyewon0531 <ifpffk@naver.com>
2025-05-28 15:15:08 +0900

    feat: 수업 리스트 타입 정의

---
+++ app/actions/get-class-info/index.ts
@@ -0,0 +1,13 @@
+/** 수업관리 탭 테이블 정보 받아오는 액션
+ * 타입만 정의해둘게요!
+ */
+
+export type ClassStatus = "수업중" | "중단" | "임시중단";
+
+export interface ClassListResponse {
+  nickName: string;
+  applicationFormId: string;
+  subject: string;
+  status: ClassStatus;
+  kakaoName: string | null;
+}
