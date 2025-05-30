713f5fc2d42d903ffca89b4a21851b8dbc9d6ea7
codekattt <widkid00@naver.com>
2025-05-30 22:42:30 +0900

    fix: 디자인 요구사항 반영해서 style 수정

--- app/components/teacher/Session/SessionComplete.tsx
+++ app/components/teacher/Session/SessionComplete.tsx
@@ -88,13 +88,16 @@ export default function SessionComplete({
     <div className="flex w-full flex-col">
       <div className="mb-5 flex flex-col gap-10">
         <TitleSection>
-          <TitleSection.Title>수업을 간략히 리뷰해주세요</TitleSection.Title>
+          <TitleSection.Title className="text-[24px]">
+            수업을 간략히 리뷰해주세요
+          </TitleSection.Title>
           <TitleSection.Description>
             작성하신 내용은 학부모님께 전달됩니다
           </TitleSection.Description>
         </TitleSection>
         <DivWithLabel
           label={`${classMinute}분 수업을 진행했나요?`}
+          labelClassName="text-[20px]"
           className="w-full"
         >
           <div className="flex gap-2">
@@ -125,7 +128,11 @@ export default function SessionComplete({
             />
           )}
         </DivWithLabel>
-        <DivWithLabel label="아이가 숙제를 모두 완료했나요?" className="w-full">
+        <DivWithLabel
+          label="아이가 숙제를 모두 완료했나요?"
+          labelClassName="text-[20px]"
+          className="w-full"
+        >
           {HOMEWORK_PROGRESS_LIST.map((item) => (
             <div key={item.value} className="py-4">
               <Radio
@@ -142,9 +149,11 @@ export default function SessionComplete({
           ))}
         </DivWithLabel>
       </div>
-      {/* 아래는 그냥 divider */}
-      <div className="-mx-5 border-t-8 border-gray-100" />
-      <DivWithLabel label="아이의 이해도" className="mb-40 mt-5">
+      <DivWithLabel
+        label="아이의 이해도"
+        labelClassName="text-[20px]"
+        className="mb-40 mt-5"
+      >
         <Textarea
           value={understanding}
           onChange={setUnderstanding}
