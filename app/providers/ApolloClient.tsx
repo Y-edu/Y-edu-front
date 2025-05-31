31995735cbd0bdd713e5fcc8fe0a06f866a91f5b
yeyounging <133792082+yeyounging@users.noreply.github.com>
2025-05-31 15:50:54 +0900

    chore

--- app/providers/ApolloClient.tsx
+++ app/providers/ApolloClient.tsx
@@ -4,9 +4,9 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { PropsWithChildren } from "react";
 
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_SERVER_URL}:8181/graphql`,
+  uri: `${process.env.NEXT_PUBLIC_SERVER_URL}:${process.env.NEXT_PUBLIC_PORT}/graphql`,
   cache: new InMemoryCache(),
-  connectToDevTools: true,
+  connectToDevTools: process.env.NODE_ENV !== "production",
 });
 
 const ApolloClientProvider = ({ children }: PropsWithChildren) => {
