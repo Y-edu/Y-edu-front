acd34da5ccbe28ba42a554e42ef8ddbfa8af10ae
yeyounging <133792082+yeyounging@users.noreply.github.com>
2025-06-01 02:13:27 +0900

    hotfix: ApolloClient 설정 변경

--- app/providers/ApolloClient.tsx
+++ app/providers/ApolloClient.tsx
@@ -3,8 +3,13 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { PropsWithChildren } from "react";
 
+const graphQLUrl =
+  process.env.NODE_ENV === "development"
+    ? `${process.env.NEXT_PUBLIC_SERVER_URL}:${process.env.NEXT_PUBLIC_PORT}/graphql`
+    : `${process.env.NEXT_PUBLIC_SERVER_URL}/graphql`;
+
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_SERVER_URL}:${process.env.NEXT_PUBLIC_PORT}/graphql`,
+  uri: graphQLUrl,
   cache: new InMemoryCache(),
   connectToDevTools: process.env.NODE_ENV !== "production",
 });
