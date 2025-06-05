1562f28d884b86528b85a556f1175ab8c6377e95
codekattt <widkid00@naver.com>
2025-06-05 17:25:50 +0900

    fix: 텍스트 수정

--- app/constants/session/homework.ts
+++ app/constants/session/homework.ts
@@ -2,6 +2,6 @@ export const HOMEWORK_PROGRESS_LIST = [
   { label: "모두 해왔어요", value: 100 },
   { label: "대부분 했어요", value: 80 },
   { label: "일부만 했어요", value: 50 },
-  { label: "거의 하지 않았어요", value: 20 },
+  { label: "안했어요", value: 20 },
   { label: "숙제가 없었어요", value: 0 },
 ] as const;
