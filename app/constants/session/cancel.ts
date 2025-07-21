e576ec70bdd3bab7a732899923ba77f723ab1c15
leehyewon0531 <ifpffk@naver.com>
2025-07-21 21:10:56 +0900

    chore: 휴강 관련 상수 문자열 추가

---
+++ app/constants/session/cancel.ts
@@ -0,0 +1,13 @@
+export const CANCEL_TEXT = {
+  SELECT_CANCEL_REASON: "휴강 사유를 선택해주세요",
+  SAME_DAY_CANCEL_BY_TEACHER_SHORT: "선생님 당일휴강",
+  SAME_DAY_CANCEL_BY_PARENTS_SHORT: "학부모 당일휴강",
+  SAME_DAY_CANCEL_BY_TEACHER_LONG: "선생님 요청에 의한 당일 휴강",
+  SAME_DAY_CANCEL_BY_PARENTS_LONG: "학부모 요청에 의한 당일 휴강",
+  SAME_DAY_CANCEL_BY_TEACHER_DESC: "별도의 무료 보강 수업을 제공해야 해요",
+  SAME_DAY_CANCEL_BY_PARENTS_DESC:
+    "진행한 수업으로 처리되어, 정산 받을 수 있어요",
+  NOT_SAME_DAY_CANCEL: "당일 이전 합의된 휴강",
+  SAME_DAY_CANCEL_ALERT: `당일 휴강은 취소할 수 없어요.\n그래도 진행할까요?`,
+  SAME_DAY_CANCEL_SUCCESS: "당일휴강 처리 됐어요",
+} as const;
