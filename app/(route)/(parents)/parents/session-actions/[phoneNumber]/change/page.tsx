d303a88b772d96ba29d0359b4e8b92beed56a492
codekattt <widkid00@naver.com>
2025-08-09 18:41:32 +0900

    feat: 학무모 일시정지, 선생님 교체 페이지 추가

---
+++ app/(route)/(parents)/parents/session-actions/[phoneNumber]/change/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import SessionActions from "@/components/parents/session-actions/SessionActions";
+import HeaderWithBack from "@/components/result/HeaderWithBack";
+
+export default function ParentsSessionActionsChangePage() {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <HeaderWithBack
+        title="Y-edu"
+        hasBack
+        onBack={() => window.history.back()}
+        mainClassName="pt-8 w-full px-5"
+      >
+        <SessionActions />
+      </HeaderWithBack>
+    </div>
+  );
+}
