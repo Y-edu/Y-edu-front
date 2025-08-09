f2104dc4ebf1120f302aa05009f3bb9872ecd3de
leehyewon0531 <ifpffk@naver.com>
2025-08-09 16:23:46 +0900

    feat: useGetSessionsMonth

---
+++ app/hooks/query/useGetSessionsMonth.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
+import {
+  getSessionsMonth,
+  SessionsMonthParams,
+} from "@/actions/get-sessions-month";
+
+export function useGetSessionsMonth(params: SessionsMonthParams) {
+  return useQuery({
+    queryKey: ["sessions-month", params],
+    queryFn: async () => {
+      const res = await getSessionsMonth(params);
+      return res;
+    },
+  });
+}
