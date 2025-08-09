13bdf24a92933ce49c650ff5eb8db70ce5273463
leehyewon0531 <ifpffk@naver.com>
2025-08-09 16:30:16 +0900

    fix: token이나 classMatchingId 둘 중 하나만 가지게 수정

--- app/actions/get-sessions-month/index.ts
+++ app/actions/get-sessions-month/index.ts
@@ -1,10 +1,17 @@
 import { httpService } from "@/utils/httpService";
 
-export interface SessionsMonthParams {
-  token: string;
-  classMatchingId: number;
-  monthCount: number; // 몇 개의 월 데이터 가져올 건지 (디폴트: 2)
-}
+// token이나 classMatchingId 둘 중 하나는 있어야 함
+export type SessionsMonthParams =
+  | {
+      token: string;
+      classMatchingId?: never; // token이 있을 때는 classMatchingId 금지
+      monthCount: number;
+    }
+  | {
+      classMatchingId: number;
+      token?: never; // classMatchingId가 있을 때는 token 금지
+      monthCount: number;
+    };
 
 export interface SessionsMonthResponse {
   months: {
@@ -13,10 +20,21 @@ export interface SessionsMonthResponse {
 }
 
 export async function getSessionsMonth(params: SessionsMonthParams) {
-  const { token, classMatchingId, monthCount } = params;
+  const { monthCount } = params;
+
+  // 공통 쿼리 파라미터
+  let query = `monthCount=${monthCount}`;
+
+  if ("token" in params) {
+    query += `&token=${params.token}`;
+  }
+
+  if ("classMatchingId" in params) {
+    query += `&classMatchingId=${params.classMatchingId}`;
+  }
 
   const res = await httpService.get<SessionsMonthResponse>(
-    `/sessions/month?token=${token}&classMatchingId=${classMatchingId}&monthCount=${monthCount}`,
+    `/sessions/month?${query}`,
   );
 
   return res.data;
