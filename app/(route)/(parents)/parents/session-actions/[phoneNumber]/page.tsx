364702ecb9d1c9642f25f619d44df17285e2f00a
codekattt <widkid00@naver.com>
2025-08-09 16:14:52 +0900

    feat: 학부모 수업상태 변경 페이지 구현

---
+++ app/(route)/(parents)/parents/session-actions/[phoneNumber]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import HeaderWithBack from "@/components/result/HeaderWithBack";
+import SessionActionsMain from "@/components/parents/session-actions/SessionActionsMain";
+
+export default function ParentsSessionActionsMainPage() {
+  return (
+    <div className="flex w-full flex-col items-center">
+      <HeaderWithBack
+        title="Y-edu"
+        hasBack
+        onBack={() => window.history.back()}
+        mainClassName="pt-8 w-full px-5"
+      >
+        <SessionActionsMain />
+      </HeaderWithBack>
+    </div>
+  );
+}
