2846b9ca93f15516c7e07cd55ffbad48ef62ee77
codekattt <widkid00@naver.com>
2025-08-17 00:02:45 +0900

    fix: next/navigation useRouter로 변경

--- app/(route)/(parents)/parents/session-actions/[phoneNumber]/page.tsx
+++ app/(route)/(parents)/parents/session-actions/[phoneNumber]/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import HeaderWithBack from "@/components/result/HeaderWithBack";
 import SessionActionsMain from "@/components/parents/session-actions/SessionActionsMain";
 
 export default function ParentsSessionActionsMainPage() {
+  const router = useRouter();
+
   return (
     <div className="flex w-full flex-col items-center">
       <HeaderWithBack
         title="Y-Edu"
         hasBack
-        onBack={() => window.history.back()}
+        onBack={() => router.back()}
         mainClassName="pt-8 w-full px-5"
       >
         <SessionActionsMain />
