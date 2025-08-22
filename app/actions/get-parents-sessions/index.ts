4cb78a78517633653c24d242d8825b574c46f508
codekattt <widkid00@naver.com>
2025-08-23 01:21:48 +0900

    fix: api 스펙 변경에 따른 코드 수정(isSubmit속성추가)

--- app/actions/get-parents-sessions/index.ts
+++ app/actions/get-parents-sessions/index.ts
@@ -1,9 +1,17 @@
 import { httpService } from "@/utils/httpService";
 
+export type SessionActionsType = "PAUSE" | "CHANGE_TEACHER";
+
+export interface SessionSubmitState {
+  PAUSE: boolean;
+  CHANGE_TEACHER: boolean;
+}
+
 export interface Session {
   sessionId: number;
   sessionDate: string;
   roundNumber: number;
+  isSubmit?: SessionSubmitState;
 }
 
 export interface ParentsSessionItem {
