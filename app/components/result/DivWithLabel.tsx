00216f58299f614e1084e1f9402c69628292f5f3
leehyewon0531 <ifpffk@naver.com>
2025-09-14 13:44:34 +0900

    fix: DivWithLabel 대소문자 처리 수정

---
+++ app/components/result/DivWithLabel.tsx
@@ -0,0 +1,32 @@
+import cn from "@/utils/cn";
+
+interface DivWithLabelProps {
+  label: React.ReactNode;
+  subLabel?: string;
+  children: React.ReactNode;
+  className?: string;
+  labelClassName?: string;
+}
+
+export default function DivWithLabel({
+  label,
+  subLabel,
+  children,
+  className,
+  labelClassName,
+}: DivWithLabelProps) {
+  return (
+    <div className={cn("flex flex-col gap-[12px]", className)}>
+      <p className="flex gap-[4px]">
+        <span className={cn("text-[16px] font-bold", labelClassName)}>
+          {label}
+        </span>
+        <span className="text-[16px] font-medium text-grey-500">
+          {subLabel}
+        </span>
+      </p>
+
+      {children}
+    </div>
+  );
+}
