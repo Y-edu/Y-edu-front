704557444760e2471bfee0567347c0cf1d8b4a3e
codekattt <widkid00@naver.com>
2025-09-25 00:22:33 +0900

    feat: 각 수업 탭 아래 수업당 분 표시 컴포넌트 추가

--- app/(route)/(teacher)/teacher/session-schedule/page.tsx
+++ app/(route)/(teacher)/teacher/session-schedule/page.tsx
@@ -7,24 +7,17 @@ import ErrorUI from "@/ui/ErrorUI";
 import HeaderWithBack from "@/components/result/HeaderWithBack";
 import SessionList from "@/components/teacher/SessionList";
 import { useGetSessions } from "@/hooks/query/useGetSessions";
+import { useGetSchedules } from "@/hooks/query/useGetSchedules";
 import TabBar from "@/ui/Bar/TabBar";
 import LoadingUI from "@/ui/LoadingUI";
-import { useGetSessionsMonth } from "@/hooks/query/useGetSessionsMonth";
 import MonthDurationNavigator from "@/components/teacher/Session/MonthDurationNavigator";
 
 export default function TeacherSessionScheduleListPage() {
   const searchParams = useSearchParams();
   const token = searchParams.get("token") ?? "";
-  const classId = searchParams.get("classId");
 
   const { data, isLoading } = useGetSessions(token, 0, 3);
-  const { data: sessionsMonthData } = useGetSessionsMonth(
-    token
-      ? { token, monthCount: 2 }
-      : classId
-        ? { classMatchingId: classId, monthCount: 2 }
-        : { token: "", monthCount: 2 },
-  );
+  const { data: schedulesData } = useGetSchedules({ token });
 
   if (isLoading) {
     return <LoadingUI />;
@@ -33,23 +26,34 @@ export default function TeacherSessionScheduleListPage() {
   const classIds = Object.keys(schedules);
   const defaultClassId =
     classIds.find((id) => schedules[id].send === true) ?? classIds[0];
-
-  const tabs = classIds.map((classId) => ({
-    trigger: classId,
-    content: <SessionList key={classId} classId={classId} />,
-  }));
-
   const currentMonth = new Date().getMonth() + 1;
+  const tabs = classIds.map((classId) => {
+    const matchedSchedule = schedulesData?.find(
+      (item) => item.applicationFormId === classId,
+    );
+    const classMatchingId = matchedSchedule?.classMatchingId;
+
+    return {
+      trigger: classId,
+      content: (
+        <div key={classId}>
+          <div className="flex items-center px-5 py-2">
+            {typeof classMatchingId === "number" && (
+              <MonthDurationNavigator
+                classId={classMatchingId}
+                defaultMonth={currentMonth}
+              />
+            )}
+          </div>
+          <SessionList classId={classId} />
+        </div>
+      ),
+    };
+  });
 
   return (
     <ErrorBoundary fallback={<ErrorUI />}>
       <HeaderWithBack title="내 과외 관리" className="border-none">
-        <div className="flex items-center px-5 py-2">
-          <MonthDurationNavigator
-            defaultMonth={currentMonth}
-            monthDuration={sessionsMonthData?.months || {}}
-          />
-        </div>
         <TabBar
           tabs={tabs}
           paramKey="classId"
